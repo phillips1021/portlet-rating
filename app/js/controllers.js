9fa070526c84ea85d83899277b4fbefb8b31fa0a
Phillips <brucephillips@ITS-Q4J87G8WN.local>
2016-01-21 12:16:45 -0600

    updated to use nested json

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,7 @@ var ratingControllers = angular.module('ratingControllers', []);
 
 ratingControllers.controller('RatingListCtrl',
   function($scope, $http) {
-    $http.get('ratings/ratings.json').success(function(data, status, headers, config) {
+    $http.get('ratings/ratings2.json').success(function(data, status, headers, config) {
       $scope.ratings = data;
     }).error(function(data, status, headers, config){
 		alert("error: " + status);
